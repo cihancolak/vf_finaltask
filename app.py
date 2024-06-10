c2a42ed5826407eba9c89e51de4f97211e243313
root <root@vultr.guest>
2024-06-10 13:35:03 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# In-memory database
+books = []
+
+# Route to get all books
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify(books), 200
+
+# Route to add a new book
+@app.route('/books', methods=['POST'])
+def add_book():
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify(new_book), 201
+
+# Route to get a book by ID
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), None)
+    if book:
+        return jsonify(book), 200
+    else:
+        return jsonify({'error': 'Book not found'}), 404
+
+# Route to update a book by ID
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), None)
+    if book:
+        data = request.get_json()
+        book.update(data)
+        return jsonify(book), 200
+    else:
+        return jsonify({'error': 'Book not found'}), 404
+
+# Route to delete a book by ID
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    global books
+    books = [book for book in books if book['id'] != book_id]
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
